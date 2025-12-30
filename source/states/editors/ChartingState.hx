1868c3f3b4fa0482a0c6d117189409320c5bcfd8
The Droid Master <82729680+TheDroidMasterDev@users.noreply.github.com>
2025-12-30 15:41:27 -0300

    Update ChartingState.hx

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -54,252 +54,51 @@ import flixel.ui.FlxButton;
 
 class ChartingState extends MusicBeatState
 {
-	public static var noteTypeList:Array<String> = 
-	[
-		'',
-		'Alt Animation',
-		'Hey!',
-		'Hurt Note',
-		'GF Sing',
-		'No Animation'
-	];
+	public static var noteTypeList:Array<String> = ['', 'Alt Animation', 'Hey!', 'Hurt Note', 'GF Sing', 'No Animation'];
 	public var ignoreWarnings = false;
-	var curNoteTypes:Array<String> = [];
-	var undos = [];
-	var redos = [];
-	var eventStuff:Array<Dynamic> =
-	[
-		['', "Nothing. Yep, that's right."],
-		['Dadbattle Spotlight', "Used in Dad Battle,\nValue 1: 0/1 = ON/OFF,\n2 = Target Dad\n3 = Target BF"],
-		['Hey!', "Plays the \"Hey!\" animation from Bopeebo,\nValue 1: BF = Only Boyfriend, GF = Only Girlfriend,\nSomething else = Both.\nValue 2: Custom animation duration,\nleave it blank for 0.6s"],
-		['Set GF Speed', "Sets GF head bopping speed,\nValue 1: 1 = Normal speed,\n2 = 1/2 speed, 4 = 1/4 speed etc.\nUsed on Fresh during the beatbox parts.\n\nWarning: Value must be integer!"],
-		['Philly Glow', "Exclusive to Week 3\nValue 1: 0/1/2 = OFF/ON/Reset Gradient\n \nNo, i won't add it to other weeks."],
-		['Kill Henchmen', "For Mom's songs, don't use this please, i love them :("],
-		['Add Camera Zoom', "Used on MILF on that one \"hard\" part\nValue 1: Camera zoom add (Default: 0.015)\nValue 2: UI zoom add (Default: 0.03)\nLeave the values blank if you want to use Default."],
-		['BG Freaks Expression', "Should be used only in \"school\" Stage!"],
-		['Trigger BG Ghouls', "Should be used only in \"schoolEvil\" Stage!"],
-		['Play Animation', "Plays an animation on a Character,\nonce the animation is completed,\nthe animation changes to Idle\n\nValue 1: Animation to play.\nValue 2: Character (Dad, BF, GF)"],
-		['Camera Follow Pos', "Value 1: X\nValue 2: Y\n\nThe camera won't change the follow point\nafter using this, for getting it back\nto normal, leave both values blank."],
-		['Alt Idle Animation', "Sets a specified suffix after the idle animation name.\nYou can use this to trigger 'idle-alt' if you set\nValue 2 to -alt\n\nValue 1: Character to set (Dad, BF or GF)\nValue 2: New suffix (Leave it blank to disable)"],
-		['Screen Shake', "Value 1: Camera shake\nValue 2: HUD shake\n\nEvery value works as the following example: \"1, 0.05\".\nThe first number (1) is the duration.\nThe second number (0.05) is the intensity."],
-		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"],
-		['Change Scroll Speed', "Value 1: Scroll Speed Multiplier (1 is default)\nValue 2: Time it takes to change fully in seconds."],
-		['Set Property', "Value 1: Variable name\nValue 2: New value"],
-		['Play Sound', "Value 1: Sound file name\nValue 2: Volume (Default: 1), ranges from 0 to 1"]
-	];
-
-	var _file:FileReference;
-	var postfix:String = '';
-	var UI_box:FlxUITabMenu;
-
-	public static var goToPlayState:Bool = false;
-	public static var curSec:Int = 0;
-	public static var lastSection:Int = 0;
-	private static var lastSong:String = '';
-
-	var bpmTxt:FlxText;
-	var camPos:FlxObject;
-	var strumLine:FlxSprite;
-	var quant:AttachedSprite;
-	var strumLineNotes:FlxTypedGroup<StrumNote>;
-	var curSong:String = 'Test';
-	var amountSteps:Int = 0;
-	var bullshitUI:FlxGroup;
-	var highlight:FlxSprite;
-
-	public static var GRID_SIZE:Int = 40;
-	var CAM_OFFSET:Int = 360;
-
-	var dummyArrow:FlxSprite;
-	var curRenderedSustains:FlxTypedGroup<FlxSprite>;
-	var curRenderedNotes:FlxTypedGroup<Note>;
-	var curRenderedNoteType:FlxTypedGroup<FlxText>;
-	var nextRenderedSustains:FlxTypedGroup<FlxSprite>;
-	var nextRenderedNotes:FlxTypedGroup<Note>;
-
-	var gridBG:FlxSprite;
-	var nextGridBG:FlxSprite;
-	var daquantspot = 0;
-	var curEventSelected:Int = 0;
-	var curUndoIndex = 0;
-	var curRedoIndex = 0;
-	var _song:SwagSong;
-	var curSelectedNote:Array<Dynamic> = null;
-	var playbackSpeed:Float = 1;
-	var vocals:FlxSound = null;
-	var leftIcon:HealthIcon;
-	var rightIcon:HealthIcon;
-	var value1InputText:FlxUIInputText;
-	var value2InputText:FlxUIInputText;
-	var currentSongName:String;
-	var zoomTxt:FlxText;
-
-	var zoomList:Array<Float> = [0.25, 0.5, 1, 2, 3, 4, 6, 8, 12, 16, 24];
-	var curZoom:Int = 2;
-
-	// MIDI VARIABLES
+	
+	// MIDI SYSTEM VARIABLES
 	var midiMenu:FlxSpriteGroup;
 	var curMidiChannel:Int = 0;
 	var midiInverted:Bool = false;
 
-	private var blockPressWhileTypingOn:Array<FlxUIInputText> = [];
-	private var blockPressWhileTypingOnStepper:Array<FlxUINumericStepper> = [];
-	private var blockPressWhileScrolling:Array<FlxUIDropDownMenu> = [];
+	var _file:FileReference;
+	var UI_box:FlxUITabMenu;
+	public static var curSec:Int = 0;
+	private static var lastSong:String = '';
 
-	var waveformSprite:FlxSprite;
-	var gridLayer:FlxTypedGroup<FlxSprite>;
-
-	public static var quantization:Int = 16;
-	public static var curQuant = 3;
-	public var quantizations:Array<Int> = [4, 8, 12, 16, 20, 24, 32, 48, 64, 96, 192];
-
-	var text:String = "";
-	public static var vortex:Bool = false;
-	public var mouseQuant:Bool = false;
+	var bpmTxt:FlxText;
+	var strumLine:FlxSprite;
+	var strumLineNotes:FlxTypedGroup<StrumNote>;
+	var _song:SwagSong;
+	
+	public static var GRID_SIZE:Int = 40;
 
 	override function create()
 	{
-		if (PlayState.SONG != null)
-			_song = PlayState.SONG;
-		else
-		{
-			Difficulty.resetList();
-			_song = {
-				song: 'Test',
-				notes: [],
-				events: [],
-				bpm: 150.0,
-				needsVoices: true,
-				player1: 'bf',
-				player2: 'dad',
-				gfVersion: 'gf',
-				speed: 1,
-				stage: 'stage'
-			};
+		if (PlayState.SONG != null) _song = PlayState.SONG;
+		else {
+			_song = { song: 'Test', notes: [], events: [], bpm: 150.0, needsVoices: true, player1: 'bf', player2: 'dad', gfVersion: 'gf', speed: 1, stage: 'stage' };
 			addSection();
-			PlayState.SONG = _song;
 		}
 
-		vortex = FlxG.save.data.chart_vortex;
-		ignoreWarnings = FlxG.save.data.ignoreWarnings;
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
-		bg.antialiasing = ClientPrefs.data.antialiasing;
-		bg.scrollFactor.set();
 		bg.color = 0xFF222222;
+		bg.scrollFactor.set();
 		add(bg);
-
-		gridLayer = new FlxTypedGroup<FlxSprite>();
-		add(gridLayer);
-
-		waveformSprite = new FlxSprite(GRID_SIZE, 0).makeGraphic(1, 1, 0x00FFFFFF);
-		add(waveformSprite);
-
-		var eventIcon:FlxSprite = new FlxSprite(-GRID_SIZE - 5, -90).loadGraphic(Paths.image('eventArrow'));
-		eventIcon.antialiasing = ClientPrefs.data.antialiasing;
-		leftIcon = new HealthIcon('bf');
-		rightIcon = new HealthIcon('dad');
-		eventIcon.scrollFactor.set(1, 1);
-		leftIcon.scrollFactor.set(1, 1);
-		rightIcon.scrollFactor.set(1, 1);
-
-		eventIcon.setGraphicSize(30, 30);
-		leftIcon.setGraphicSize(0, 45);
-		rightIcon.setGraphicSize(0, 45);
-
-		add(eventIcon);
-		add(leftIcon);
-		add(rightIcon);
-
-		leftIcon.setPosition(GRID_SIZE + 10, -100);
-		rightIcon.setPosition(GRID_SIZE * 5.2, -100);
-
-		curRenderedSustains = new FlxTypedGroup<FlxSprite>();
-		curRenderedNotes = new FlxTypedGroup<Note>();
-		curRenderedNoteType = new FlxTypedGroup<FlxText>();
-		nextRenderedSustains = new FlxTypedGroup<FlxSprite>();
-		nextRenderedNotes = new FlxTypedGroup<Note>();
-
-		FlxG.mouse.visible = true;
-		currentSongName = Paths.formatToSongPath(_song.song);
-		loadSong();
-		reloadGridLayer();
-		Conductor.bpm = _song.bpm;
-		Conductor.mapBPMChanges(_song);
-		if(curSec >= _song.notes.length) curSec = _song.notes.length - 1;
-
-		bpmTxt = new FlxText(10, 100, 0, "", 16);
-		bpmTxt.scrollFactor.set();
-		add(bpmTxt);
 
 		strumLine = new FlxSprite(0, 50).makeGraphic(Std.int(GRID_SIZE * 9), 4);
 		add(strumLine);
 
-		quant = new AttachedSprite('chart_quant','chart_quant');
-		quant.animation.addByPrefix('q','chart_quant',0,false);
-		quant.animation.play('q', true, false, 0);
-		quant.sprTracker = strumLine;
-		quant.xAdd = -32;
-		quant.yAdd = 8;
-		add(quant);
-		
-		strumLineNotes = new FlxTypedGroup<StrumNote>();
-		for (i in 0...8) {
-			var note:StrumNote = new StrumNote(GRID_SIZE * (i+1), strumLine.y, i % 4, 0);
-			note.setGraphicSize(GRID_SIZE, GRID_SIZE);
-			note.updateHitbox();
-			note.playAnim('static', true);
-			strumLineNotes.add(note);
-			note.scrollFactor.set(1, 1);
-		}
-		add(strumLineNotes);
-
-		camPos = new FlxObject(0, 0, 1, 1);
-		camPos.setPosition(strumLine.x + CAM_OFFSET, strumLine.y);
-
-		dummyArrow = new FlxSprite().makeGraphic(GRID_SIZE, GRID_SIZE);
-		dummyArrow.antialiasing = ClientPrefs.data.antialiasing;
-		add(dummyArrow);
-
-		var tabs = [
-			{name: "Song", label: 'Song'},
-			{name: "Section", label: 'Section'},
-			{name: "Note", label: 'Note'},
-			{name: "Events", label: 'Events'},
-			{name: "Charting", label: 'Charting'},
-			{name: "Data", label: 'Data'},
-		];
-
+		var tabs = [{name: "Song", label: 'Song'}, {name: "Section", label: 'Section'}];
 		UI_box = new FlxUITabMenu(null, tabs, true);
 		UI_box.resize(300, 400);
-		UI_box.x = 640 + GRID_SIZE / 2;
+		UI_box.x = 640;
 		UI_box.y = 25;
-		UI_box.scrollFactor.set();
-
 		add(UI_box);
+
 		addSongUI();
-		addSectionUI();
-		addNoteUI();
-		addEventsUI();
-		addDataUI();
-		addChartingUI();
-		updateHeads();
-		updateWaveform();
-
-		add(curRenderedSustains);
-		add(curRenderedNotes);
-		add(curRenderedNoteType);
-		add(nextRenderedSustains);
-		add(nextRenderedNotes);
-
-		if(lastSong != currentSongName) changeSection();
-		lastSong = currentSongName;
-
-		zoomTxt = new FlxText(10, 100-16, 0, "Zoom: 1 / 1", 16);
-		zoomTxt.scrollFactor.set();
-		add(zoomTxt);
 		
-		updateGrid();
-
 		#if android
 		addVirtualPad(CHART_EDITOR, CHART_EDITOR);
 		#end
@@ -309,86 +108,13 @@ class ChartingState extends MusicBeatState
 
 	function addSongUI():Void
 	{
-		UI_songTitle = new FlxUIInputText(10, 10, 70, _song.song, 8);
-		UI_songTitle.focusGained = () -> FlxG.stage.window.textInputEnabled = true;
-		blockPressWhileTypingOn.push(UI_songTitle);
-		
-		var check_voices = new FlxUICheckBox(10, 25, null, null, "Has voice track", 100);
-		check_voices.checked = _song.needsVoices;
-		check_voices.callback = function() { _song.needsVoices = check_voices.checked; };
-
-		var saveButton:FlxButton = new FlxButton(110, 8, "Save", function() { saveLevel(); });
-		var reloadSong:FlxButton = new FlxButton(saveButton.x + 90, saveButton.y, "Reload Audio", function() {
-			currentSongName = Paths.formatToSongPath(UI_songTitle.text);
-			loadSong();
-			updateWaveform();
-		});
-
-		var reloadSongJson:FlxButton = new FlxButton(reloadSong.x, saveButton.y + 30, "Reload JSON", function() {
-			openSubState(new Prompt('This action will clear current progress.\n\nProceed?', 0, function() {
-				loadJson(_song.song.toLowerCase());
-			}, null, ignoreWarnings));
-		});
-
-		var loadAutosaveBtn:FlxButton = new FlxButton(reloadSongJson.x, reloadSongJson.y + 30, 'Load Autosave', function() {
-			PlayState.SONG = Song.parseJson(FlxG.save.data.autosave);
-			MusicBeatState.resetState();
-		});
-
-		var loadEventJson:FlxButton = new FlxButton(loadAutosaveBtn.x, loadAutosaveBtn.y + 30, 'Load Events', function() {
-			var songName:String = Paths.formatToSongPath(_song.song);
-			var file:String = Paths.json(songName + '/events');
-			#if MODS_ALLOWED
-			if (FileSystem.exists(Paths.modsJson(songName + '/events')) || FileSystem.exists(SUtil.getPath() + file))
-			#else
-			if (OpenFlAssets.exists(file))
-			#end
-			{
-				clearEvents();
-				var events:SwagSong = Song.loadFromJson('events', songName);
-				_song.events = events.events;
-				changeSection(curSec);
-			}
-		});
-
-		// MIDI BUTTON INTEGRADO
-		var loadMidiBtn:FlxButton = new FlxButton(loadEventJson.x, loadEventJson.y + 30, "Midi 2 Chart", function() {
-			openMidiMenu();
-		});
-
-		var saveEvents:FlxButton = new FlxButton(110, reloadSongJson.y, 'Save Events', function () { saveEvents(); });
-
-		var stepperBPM:FlxUINumericStepper = new FlxUINumericStepper(10, 70, 1, 1, 1, 400, 3);
-		stepperBPM.value = Conductor.bpm;
-		stepperBPM.name = 'song_bpm';
-		blockPressWhileTypingOnStepper.push(stepperBPM);
-
-		var stepperSpeed:FlxUINumericStepper = new FlxUINumericStepper(10, stepperBPM.y + 35, 0.1, 1, 0.1, 10, 2);
-		stepperSpeed.value = _song.speed;
-		stepperSpeed.name = 'song_speed';
-		blockPressWhileTypingOnStepper.push(stepperSpeed);
-
-		var characters:Array<String> = Mods.mergeAllTextsNamed('data/characterList.txt', Paths.getPreloadPath());
-		var player1DropDown = new FlxUIDropDownMenu(10, stepperSpeed.y + 45, FlxUIDropDownMenu.makeStrIdLabelArray(characters, true), function(character:String) {
-			_song.player1 = characters[Std.parseInt(character)];
-			updateHeads();
-		});
-		player1DropDown.selectedLabel = _song.player1;
-
 		var tab_group_song = new FlxUI(null, UI_box);
 		tab_group_song.name = "Song";
-		tab_group_song.add(UI_songTitle);
-		tab_group_song.add(check_voices);
-		tab_group_song.add(saveButton);
-		tab_group_song.add(saveEvents);
-		tab_group_song.add(reloadSong);
-		tab_group_song.add(reloadSongJson);
-		tab_group_song.add(loadAutosaveBtn);
-		tab_group_song.add(loadEventJson);
+
+		var loadMidiBtn:FlxButton = new FlxButton(10, 10, "Midi 2 Chart", function() {
+			openMidiMenu();
+		});
 		tab_group_song.add(loadMidiBtn);
-		tab_group_song.add(stepperBPM);
-		tab_group_song.add(stepperSpeed);
-		tab_group_song.add(player1DropDown);
 
 		UI_box.addGroup(tab_group_song);
 	}
@@ -396,44 +122,41 @@ class ChartingState extends MusicBeatState
 	override function update(elapsed:Float)
 	{
 		updateMidiTouch();
-
-		if (FlxG.sound.music != null)
-			Conductor.songPosition = FlxG.sound.music.time;
-
 		super.update(elapsed);
 	}
 
-	// MIDI SYSTEM FUNCTIONS
 	function openMidiMenu() {
 		if(midiMenu != null) midiMenu.destroy();
 		midiMenu = new FlxSpriteGroup();
-		midiMenu.scrollFactor.set();
-
-		var bg:FlxSprite = new FlxSprite().makeGraphic(500, 420, 0xEE111111);
+		
+		var bg:FlxSprite = new FlxSprite().makeGraphic(500, 400, 0xEE000000);
 		bg.screenCenter();
 		midiMenu.add(bg);
 
-		var title:FlxText = new FlxText(bg.x, bg.y + 20, 500, "GERADOR MIDI-2-CHART", 24);
-		title.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE, CENTER);
-		midiMenu.add(title);
+		var txt:FlxText = new FlxText(bg.x, bg.y + 20, 500, "MIDI IMPORT", 20);
+		txt.setFormat(null, 20, 0xFFFFFF, CENTER);
+		midiMenu.add(txt);
 
-		var btnCanal:FlxSprite = new FlxSprite(bg.x + 50, bg.y + 100).makeGraphic(400, 60, 0xFFFF4444);
-		midiMenu.add(btnCanal);
-		var txtCanal:FlxText = new FlxText(btnCanal.x, btnCanal.y + 15, 400, "CANAL MIDI: " + curMidiChannel, 22);
-		txtCanal.setFormat(Paths.font("vcr.ttf"), 22, FlxColor.WHITE, CENTER);
-		midiMenu.add(txtCanal);
+		// Botão Canal (Membro 2 e 3)
+		var b1 = new FlxSprite(bg.x + 50, bg.y + 80).makeGraphic(400, 50, 0xFFFF4444);
+		midiMenu.add(b1);
+		var t1 = new FlxText(b1.x, b1.y + 10, 400, "CANAL: " + curMidiChannel, 18);
+		t1.alignment = CENTER;
+		midiMenu.add(t1);
 
-		var btnInv:FlxSprite = new FlxSprite(bg.x + 50, bg.y + 190).makeGraphic(400, 60, 0xFF4444FF);
-		midiMenu.add(btnInv);
-		var txtInv:FlxText = new FlxText(btnInv.x, btnInv.y + 15, 400, "LADO: OPPONENT", 22);
-		txtInv.setFormat(Paths.font("vcr.ttf"), 22, FlxColor.WHITE, CENTER);
-		midiMenu.add(txtInv);
+		// Botão Lado (Membro 4 e 5)
+		var b2 = new FlxSprite(bg.x + 50, bg.y + 160).makeGraphic(400, 50, 0xFF4444FF);
+		midiMenu.add(b2);
+		var t2 = new FlxText(b2.x, b2.y + 10, 400, "LADO: OPPONENT", 18);
+		t2.alignment = CENTER;
+		midiMenu.add(t2);
 
-		var btnConf:FlxSprite = new FlxSprite(bg.x + 50, bg.y + 300).makeGraphic(400, 80, 0xFF44FF44);
-		midiMenu.add(btnConf);
-		var txtConf:FlxText = new FlxText(btnConf.x, btnConf.y + 25, 400, "GERAR CHART AGORA", 26);
-		txtConf.setFormat(Paths.font("vcr.ttf"), 26, FlxColor.BLACK, CENTER);
-		midiMenu.add(txtConf);
+		// Botão Iniciar (Membro 6 e 7)
+		var b3 = new FlxSprite(bg.x + 50, bg.y + 260).makeGraphic(400, 70, 0xFF44FF44);
+		midiMenu.add(b3);
+		var t3 = new FlxText(b3.x, b3.y + 20, 400, "CONVERTER", 22);
+		t3.alignment = CENTER;
+		midiMenu.add(t3);
 
 		add(midiMenu);
 	}
@@ -442,18 +165,15 @@ class ChartingState extends MusicBeatState
 		if (midiMenu != null && midiMenu.visible && FlxG.mouse.justPressed) {
 			if (FlxG.mouse.overlaps(midiMenu.members[2])) {
 				curMidiChannel = (curMidiChannel + 1) % 16;
-				cast(midiMenu.members[3], FlxText).text = "CANAL MIDI: " + curMidiChannel;
-				FlxG.sound.play(Paths.sound('scrollMenu'));
+				cast(midiMenu.members[3], FlxText).text = "CANAL: " + curMidiChannel;
 			}
 			if (FlxG.mouse.overlaps(midiMenu.members[4])) {
 				midiInverted = !midiInverted;
 				cast(midiMenu.members[5], FlxText).text = "LADO: " + (midiInverted ? "PLAYER" : "OPPONENT");
-				FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
 			if (FlxG.mouse.overlaps(midiMenu.members[6])) {
 				processMidiToChart();
 				midiMenu.visible = false;
-				FlxG.sound.play(Paths.sound('confirmMenu'));
 			}
 		}
 	}
@@ -463,58 +183,36 @@ class ChartingState extends MusicBeatState
 		#if sys
 		if(FileSystem.exists(path)) {
 			var bytes:Bytes = File.getBytes(path);
-			
-			// Limpa o chart
 			for (sec in _song.notes) sec.sectionNotes = [];
 
-			// PARSER BINÁRIO SIMPLIFICADO
-			var i:Int = 14; // Header offset
+			var i:Int = 14; 
 			while(i < bytes.length - 3) {
 				var status = bytes.get(i);
 				if ((status & 0xF0) == 0x90) { // Note On
-					var chan = status & 0x0F;
 					var pitch = bytes.get(i + 1);
 					var vel = bytes.get(i + 2);
-					
-					if (chan == curMidiChannel && vel > 0) {
+					if (vel > 0) {
 						var time = (i / bytes.length) * FlxG.sound.music.length;
 						var lane = pitch % 4;
 						if (midiInverted) lane += 4;
-						
-						var sec = Std.int(time / (Conductor.stepCrochet * 16));
-						if(_song.notes[sec] != null) {
-							_song.notes[sec].sectionNotes.push([time, lane, 0]);
-						}
+						var secIdx = Std.int(time / (Conductor.stepCrochet * 16));
+						if(_song.notes[secIdx] != null) _song.notes[secIdx].sectionNotes.push([time, lane, 0]);
 					}
 					i += 3;
-				} else { i++; }
+				} else i++;
 			}
 			updateGrid();
-			FlxG.log.add("MIDI importado com sucesso!");
-		} else {
-			trace("Arquivo song.mid nao encontrado em assets/midi2charts/");
 		}
 		#end
 	}
 
-	// Funções extras necessárias para compilar o código fornecido
-	function addSectionUI() {}
-	function addNoteUI() {}
-	function addEventsUI() {}
-	function addDataUI() {}
-	function addChartingUI() {}
-	function updateHeads() {}
-	function updateWaveform() {}
-	function loadSong() {}
-	function reloadGridLayer() {}
-	function addSection() {}
-	function updateGrid() {
-		// Esta função atualiza o grid visual
-		super.update(0);
+	function addSection(lengthInSteps:Int = 16):Void {
+		var sec:Section = { sectionNotes: [], lengthInSteps: lengthInSteps, mustHitSection: true };
+		_song.notes.push(sec);
 	}
-	function saveLevel() {}
-	function saveEvents() {}
-	function clearEvents() {}
-	function loadJson(name:String) {}
-}
 
+	function updateGrid():Void {
+		// Recarrega o grid visualmente
+		MusicBeatState.resetState();
+	}
+}
